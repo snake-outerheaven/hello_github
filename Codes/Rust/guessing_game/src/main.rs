870d38025d41fc1cf3b313dbfb3f488148406489
neo-dev <sei133dirsoa@gmail.com>
2025-07-18 03:34:25 -0300

    Adicionando nova funcionalidade no jogo da adivinhação

--- Codes/Rust/guessing_game/src/main.rs
+++ Codes/Rust/guessing_game/src/main.rs
@@ -9,7 +9,6 @@ use std::process::Command; // estrutura Command usada para rodar comandos do she
 use std::thread::sleep; // função que pausa o código, da biblioteca de manipulação de threads
 use std::time::Duration; // estrutura que me permite manipular o tempo, usada principalmente no sleep
 // vem da biblioteca padrão de tipos temporais ( não sei melhor forma de descrever)
-
 // função que busca limpar tela de forma portátil no Windows e sistemas Unix-like
 
 fn limpar_tela() {
@@ -32,6 +31,9 @@ fn obtendo_palpite() -> u32 {
     println!("Digite o seu palpite para o número misterioso:");
     let mut palpite = String::new();
     loop {
+        palpite.clear();
+        // faltou limpar palpite a cada iteração, causando um bug de lógica, pois read_line insere o conteúdo do buffer, não limpando
+        // entradas anteriores
         io::stdin()
             .read_line(&mut palpite)
             .expect("crash and burn! ( falha ao ler o palpite )");
@@ -42,13 +44,43 @@ fn obtendo_palpite() -> u32 {
                 return palpite;
             }
             Ok(_) => {
-                println!("Por favor, digite um número válido!");
+                println!("Por favor, digite um número entre 1 e 100!");
             }
             Err(_) => {
                 println!("Por favor, digite um número válido!");
             }
         };
     } //loop
+}
+
+// aqui vem a função que modulariza a obtenção de input do usuário
+
+fn verif(tentativas: u32) -> bool {
+    sleep(Duration::from_millis(300));
+    println!("Você tentou {tentativas} vezes, deseja continuar? (S/N)");
+    let mut entrada = String::new();
+    io::stdin()
+        .read_line(&mut entrada)
+        .expect("crash and burn! não foi possível ler stdin.");
+
+    match entrada.trim().to_uppercase().as_str() {
+        "S" => {
+            sleep(Duration::from_millis(250));
+            println!("Certo, vamos continuar o jogo!");
+            return false;
+        }
+
+        "N" => {
+            sleep(Duration::from_millis(250));
+            println!("Certo, obrigado por jogar este jogo!");
+            return true;
+        }
+        _ => {
+            sleep(Duration::from_millis(250));
+            println!("Resposta inválida! Favor responder S ou N.");
+            return verif(tentativas); // recursão mesmo, nao acho que vai pesar muito na stack
+        }
+    }
 }
 
 fn main() {
@@ -62,26 +94,32 @@ fn main() {
     // é diferente, thread_rng faz a mesma coisa usando as fontes de entropia do meu sistema ( caos ) para gerar um número
     // e o gen_range apenas limita o alcance da geração de números aleatórios para o intervalo de 1 a 100
 
-    loop {
+    let mut usuario_quer_parar = false;
+
+    let mut tentativas: u32 = 0;
+
+    while !usuario_quer_parar {
+        tentativas += 1;
         let palpite: u32 = obtendo_palpite();
 
         match palpite.cmp(&numero_secreto) {
             Ordering::Less => {
                 sleep(Duration::from_millis(500));
                 println!("{palpite} é menor que o número secreto!");
-                continue;
             }
             Ordering::Equal => {
                 sleep(Duration::from_millis(500));
-                println!("Bingo! Você adivinhou o número secreto!");
+                println!("Bingo! Você adivinhou o número secreto com {tentativas}. Parabéns!");
                 return;
             }
             Ordering::Greater => {
                 sleep(Duration::from_millis(500));
                 println!("{palpite} é maior que o número secreto!");
-                continue;
-            }
+            } // vou fazer uma função vazia que executa o que quero fazer
         };
+
+        // verificação se o usuário quer parar ou continuar
+        usuario_quer_parar = verif(tentativas);
 
         // comparar números é verboso, mas a forma como rust aborda as coisas não é muito difícil de entender, agora é seguir lendo o livro
     } // fim do loop aqui
