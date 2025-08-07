333ff6da5263a65b952e9b9238480e777902ce9b
neo-dev <sei133dirsoa@gmail.com>
2025-08-07 00:36:06 -0300

    Adicionando projeto de jogo de adivinhação de números em Java, com planos de expandir para GUI.

---
+++ Codes/Java/GuessingGame.java
@@ -0,0 +1,129 @@
+import java.io.*; // importa classes de leitura e escrita de arquivos
+import java.util.Random; // utilidade de geração de números pseudoaleatórios
+import java.util.Scanner; // classe que para escanear entrada do teclado
+
+
+public class GuessingGame {
+
+    // Objeto que representa o meu conversor de temperatura
+
+
+
+    static Scanner scanner = new Scanner(System.in); // crio objeto scanner global para todos os métodos do programa
+
+    // keyword static indica que o método funciona como uma função, ainda não toquei no OOP propriamente dito, mas apenas esotu
+    // me acostumando com a sintaxe da linguagem.
+    
+    public static void limpartela () {
+
+        // método simples que tenta conter o componente de limpar tela do programa.
+
+        // Parte mais importante deste método, tratamento de exceções ( erros )
+        
+        try {
+            System.out.println("Limpando tela..."); // Imprimindo no terminal
+            Thread.sleep(750); // Método para fazer o processo do programa dormir, número em milisegundos
+
+            if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+
+                new ProcessBuilder("cmd","/c","cls").inheritIO().start().waitFor(); 
+                // construtor de Processos, que herda o stdin e stdout do programa, inicia, e espera até o processo iniciado
+                // seja encerrado
+
+            } else {
+
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+
+            }
+
+        }
+
+        //tenta isso, se não, roda o abaixo
+        //
+        // a linha abaixo significa de forma literal, que ele vai pegar alguma exceção no programa
+
+        catch (Exception e) {
+
+            System.out.println("Não foi possível limpar tela.");
+
+        }
+
+    } // fim do método aqui
+
+
+    // sabendo que os métodos sempre retornam alguma coisa (tipo void é válido aqui), vamos obter o nome do usuário
+
+    public static String obter_nome() {
+
+        // tratamento de erros para checar se o processo consegue dormir
+
+        try {
+
+            Thread.sleep(750);
+
+        }
+
+        catch (Exception e) {
+
+            System.out.println("Não foi possível gera pausa visual para UX");
+        }
+
+        // loop principal, bem tranquilo
+
+        while(true) {
+
+            System.out.println("Este programa necessita de seu nome de usuário, por favor, digite-o abaixo.");
+
+            String nome = scanner.nextLine().trim();
+            // a variável nome é inicializada ao se fazer necessária, o que deixa o código meio diferente do estilo C, mesmo Java sendo descendente
+            // desta família de linguagens.
+
+            // a forma de representar strings formatadas em stdout é quase a mesma coisa de C
+
+            System.out.printf("O nome %s está correto? (S/N)\n", nome);
+
+            String resposta = scanner.nextLine().trim().toUpperCase();
+
+            switch (resposta) {
+
+                case "S":
+                    System.out.printf("Certo, bem vindo ao programa %s!", nome);
+                    return nome;
+
+                case "N":
+
+                    System.out.printf("Certo, entendi que %s não é o seu nome, repetindo o código...\n", nome);            
+
+                    try {
+                        Thread.sleep(750);
+                    }
+                    catch (Exception e) {
+                        System.out.println("Não foi possível gera pausa visual para UX");
+                    }
+                    limpartela();
+
+                    break;
+
+                default:
+
+                    System.out.println("Reiniciando a função por motivos de segurança...");
+
+                    limpartela();
+
+                    break;
+            
+            }
+
+        }
+
+    }
+    
+    // método principal ( em OOP puro, eu criaria uma classe separada onde os objetos são criados e começam a interagir entre si )
+    public static void main(String[] args){
+        limpartela();
+        String nome = obter_nome();
+        System.out.printf("\nNome do usuário é: %s\n", nome);
+    }
+
+
+}
